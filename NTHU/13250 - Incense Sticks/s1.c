54e5b59d131e43d44ad0fca743d0c9e5de74119b
punkfulw <81062498+punkfulw@users.noreply.github.com>
2021-09-28 11:31:50 +0800

    13250

---
+++ NTHU/13250 - Incense Sticks/s1.c
@@ -0,0 +1,14 @@
+// OJ: https://acm.cs.nthu.edu.tw/problem/13250/
+// Author: github.com/punkfulw
+// Time: O(1)
+// Space: O(1)
+
+#include<stdio.h>
+
+int main(){
+    printf(" \\|/ \\|/ \\|/ \n");
+    printf("(\"_____\")\n");
+    printf("( ^ ^)( ^ ^)\n");
+
+    return 0;
+}
