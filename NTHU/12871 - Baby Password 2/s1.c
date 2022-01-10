0d350573bd88e5b327940825c56ecba6fc800c29
punkfulw <81062498+punkfulw@users.noreply.github.com>
2022-01-10 11:27:35 +0800

    12871

---
+++ NTHU/12871 - Baby Password 2/s1.c
@@ -0,0 +1,22 @@
+// OJ: https://acm.cs.nthu.edu.tw/problem/12871/
+// Author: github.com/punkfulw
+// Time: O(1)
+// Space: O(1)
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+
+int main(){
+	char c;
+    int shift;
+    scanf("%c%d", &c, &shift);
+    if (isupper(c))
+        c = ((c - 'A') - shift + 26) % 26 + 'A';
+    else 
+        c = ((c - 'a') - shift + 26) % 26 + 'a';
+    c ^= 32;
+    printf("%c\n", c);
+    return 0;
+}
